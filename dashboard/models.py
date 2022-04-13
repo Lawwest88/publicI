56f5db55ff48ce3fd31761ac3a8a51bbf237cc17
Lawwest88 <lawrence-west@hotmail.com>
2022-04-13 01:08:11 -0500

    created app

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class dashboard(models.Model):
+    # user
+    first_name  = models.CharField(max_length=50)
+    last_name   = models.CharField(max_length=50)
+    age = models.IntegerField(null=True)
+
+    # incident
+    fire = models.CharField(max_length=50)
+    police = models.CharField(max_length=50)
+    medical = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now=True)
+
+    # maps
+    getmap = models.URLField(max_length=200)
+    pinned_map = models.URLField(max_length=200)
